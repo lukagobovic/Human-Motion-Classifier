645e8e5fbdcda6b301fa8311f9fee6ed6e61db79
Luka Gobovic <lukagobovic@gmail.com>
2023-03-16 12:16:25 -0400

    worked on stuff

--- test.py
+++ test.py
@@ -7,18 +7,18 @@ from matplotlib import style
 
 style.use('ggplot')
 
-xaxis, x, y, z, a = np.loadtxt('Raw Data walking jacket.csv', delimiter = ',', unpack = True)
+xaxis, xAccel, y, z, a = np.loadtxt('5_sec_jumping_front.csv', delimiter = ',', unpack = True)
 
 def graph():
-	plt.plot(xaxis,x)
-	#plt.plot(xaxis,y)
+	#plt.plot(xaxis,xAccel)
+	plt.plot(xaxis,y)
 	#plt.plot(xaxis,z)
-	#plt.plot(xaxis,a)
+  #plt.plot(xaxis,a)
 
 
 	plt.title('Accelerometer Data Collected with SensorLog App on iPhone 6S')
-	plt.ylabel('')
-	plt.xlabel('7 Everyday Activites')
+	plt.xlabel('time')
+	plt.ylabel('Acceleration')
 
 	plt.show()
 
