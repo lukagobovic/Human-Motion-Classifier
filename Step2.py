4fce938bf4129dd695cbf9e8981b68b0ab077031
Luka Gobovic <19lg26@queensu.ca>
2023-04-06 21:48:36 -0400

    working on parts 3,4,5

--- Step2.py
+++ Step2.py
@@ -5,7 +5,8 @@ from scipy.signal import savgol_filter
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
 
 LukaWalkingData = pd.concat(
@@ -32,103 +33,166 @@ all_data = {
     'Bennett': {'walking': BennettWalkingData, 'jumping': BennettJumpingData},
     'CJ': {'walking': CJWalkingData, 'jumping': CJJumpingData}
 }
-
-# Concatenate walking and jumping data
 combinedWalkingData = pd.concat([LukaWalkingData, BennettWalkingData, CJWalkingData], ignore_index=True)
 combinedJumpingData = pd.concat([LukaJumpingData, BennettJumpingData, CJJumpingData], ignore_index=True)
-
 combined_data = pd.concat([combinedWalkingData, combinedJumpingData], ignore_index=True)
 
-window_size = 5 * 100  # 5 seconds at 100 Hz sampling rate
-num_windows = len(combined_data) // window_size
-segmented_data = [combined_data[i*window_size : (i+1)*window_size] for i in range(num_windows)]
+#Concatenate walking and jumping data
+combinedWalkingData = combinedWalkingData.to_numpy()
+np.random.shuffle(combinedWalkingData)
+walking_combined_df = pd.DataFrame(combinedWalkingData)
 
-np.random.shuffle(segmented_data)
+combinedJumpingData = combinedJumpingData.to_numpy()
+np.random.shuffle(combinedJumpingData)
+walking_combined_df = pd.DataFrame(combinedJumpingData)
 
-shuffled_data = pd.concat(segmented_data, ignore_index=True)
+combined_data = combined_data.to_numpy()
+np.random.shuffle(combined_data)
+test_combined_df = pd.DataFrame(combined_data)
 
-train_data, test_data = train_test_split(shuffled_data, test_size=0.1, random_state=42)
-print(train_data)
+with h5py.File('./ProjectFile.hdf5', 'w') as f:
+    # Create sub groups for each member
+    for member_name, member_data in all_data.items():
+        member_group = f.create_group(member_name)
+        member_group.create_dataset('walking', data=member_data['walking'])
+        member_group.create_dataset('jumping', data=member_data['jumping'])
+
+    # Create a sub group for the dataset
+    dataset_group = f.create_group('dataset')
+
+    # Segment and shuffle all accelerometer data
+    all_segments = []
+    for member_name, member_data in all_data.items():
+        for activity in ['walking', 'jumping']:
+            data = member_data[activity]
+
+            # Segment the data into 5-second windows
+            num_segments = (len(data) - 500) // 100 + 1
+            segments = [data[(i * 100):(i * 100 + 500)] for i in range(num_segments)]
+
+            # Label the segments with the member name and activity
+            labels = [f'{member_name}_{activity}' for _ in range(num_segments)]
+
+            # Combine the segments and labels for this position/activity combination
+            all_segments.extend(list(zip(segments, labels)))
+
+    # Shuffle the segmented data
+    np.random.shuffle(all_segments)
+
+    # Split the segmented data into 90% train and 10% test
+    num_train = int(0.9 * len(all_segments))
+    train_segments = all_segments[:num_train]
+    test_segments = all_segments[num_train:]
+
+    # Create sub groups for train and test datasets
+    train_group = dataset_group.create_group('train')
+    test_group = dataset_group.create_group('test')
+
+    # Add walking and jumping datasets to train and test sub-groups
+    train_group.create_dataset('walking', data=[seg[0] for seg in train_segments if 'walking' in seg[1]])
+    train_group.create_dataset('jumping', data=[seg[0] for seg in train_segments if 'jumping' in seg[1]])
+    test_group.create_dataset('walking', data=[seg[0] for seg in test_segments if 'walking' in seg[1]])
+    test_group.create_dataset('jumping', data=[seg[0] for seg in test_segments if 'jumping' in seg[1]])
 
 
-# train_indices = window_indices[:int(0.9 * num_windows)]
-# test_indices = window_indices[int(0.9 * num_windows):]
-# print(train_indices)
+# # Load data from HDF5 file
+# with h5py.File('combined_data.hdf5', 'r') as f:
+#     train_X = f['train_X'][:]
+#     train_y = f['train_y'][:]
+#     test_X = f['test_X'][:]
+#     test_y = f['test_y'][:]
 
-# train_data_start = train_indices[0] * window_size
-# train_data_end = (train_indices[-1] + 1) * window_size
-# train_data = combined_data.iloc[train_data_start:train_data_end, :]
+# # Apply moving average filter
+# # train_X = savgol_filter(train_X, window_length=5, polyorder=3, axis=1)
+# # test_X = savgol_filter(test_X, window_length=5, polyorder=3, axis=1)
 
-# test_data_start = test_indices[0] * window_size
-# test_data_end = (test_indices[-1] + 1) * window_size
-# test_data = combined_data.iloc[test_data_start:test_data_end, :]
-# print(test_data)
+# # # Detect and remove outliers
+# # train_X = train_X[~((train_X - np.mean(train_X, axis=1, keepdims=True)) > 2 * np.std(train_X, axis=1, keepdims=True))]
+# # train_y = train_y[~((train_X - np.mean(train_X, axis=1, keepdims=True)) > 2 * np.std(train_X, axis=1, keepdims=True))]
+# # test_X = test_X[~((test_X - np.mean(test_X, axis=1, keepdims=True)) > 2 * np.std(test_X, axis=1, keepdims=True))]
+# # test_y = test_y[~((test_X - np.mean(test_X, axis=1, keepdims=True)) > 2 * np.std(test_X, axis=1, keepdims=True))]
+
+# # # Normalize the data
+# # train_X = (train_X - np.mean(train_X)) / np.std(train_X)
+# # test_X = (test_X - np.mean(test_X)) / np.std(test_X)
+
+# # Define a function to extract features
+# # def extract_features(data):
+# #     features = []
+# #     for i in range(data.shape[0]):
+# #         window = data[i, :]
+# #         feature = []
+# #         feature.append(np.max(window[:, 0]))  # Maximum acceleration in x-direction
+# #         feature.append(np.max(window[:, 1]))  # Maximum acceleration in y-direction
+# #         feature.append(np.max(window[:, 2]))  # Maximum acceleration in z-direction
+# #         feature.append(np.min(window[:, 0]))  # Minimum acceleration in x-direction
+# #         feature.append(np.min(window[:, 1]))  # Minimum acceleration in y-direction
+# #         feature.append(np.min(window[:, 2]))  # Minimum acceleration in z-direction
+# #         feature.append(np.mean(window[:, 0]))  # Mean acceleration in x-direction
+# #         feature.append(np.mean(window[:, 1]))  # Mean acceleration in y-direction
+# #         feature.append(np.mean(window[:, 2]))  # Mean acceleration in z-direction
+# #         feature.append(np.std(window[:, 0]))  # Standard deviation of acceleration in x-direction
+# #         feature.append(np.std(window[:, 1]))  # Standard deviation of acceleration in y-direction
+# #         feature.append(np.std(window[:, 2]))  # Standard deviation of acceleration in z-direction
+# #         features.append(feature)
+# #     return np.array(features)
+
+# def extract_features(data):
+#     # Create an empty array to hold the feature vectors
+#     features = np.zeros((data.shape[0], 12))
+
+#     # Iterate over each data row and extract the features
+#     for i in range(data.shape[0]):
+#         # Extract the data from the row
+#         row_data = data[i, 1:4]  # extract data from columns 2, 3, and 4
+
+#         # Compute the features
+#         max_x = np.max(row_data[0])
+#         max_y = np.max(row_data[1])
+#         max_z = np.max(row_data[2])
+#         min_x = np.min(row_data[0])
+#         min_y = np.min(row_data[1])
+#         min_z = np.min(row_data[2])
+#         mean_x = np.mean(row_data[0])
+#         mean_y = np.mean(row_data[1])
+#         mean_z = np.mean(row_data[2])
+#         std_x = np.std(row_data[0])
+#         std_y = np.std(row_data[1])
+#         std_z = np.std(row_data[2])
+
+#         # Store the features in the features array
+#         features[i, :] = (max_x, max_y, max_z, min_x, min_y, min_z, mean_x, mean_y, mean_z, std_x, std_y, std_z)
+
+#     # Create a column of labels
+#     labels = np.ones((data.shape[0], 1))
+
+#     # Add the labels column to the feature array
+#     all_features = np.hstack((features, labels))
+
+#     return all_features
+
+# # Extract features from training and testing sets
+# train_features = extract_features(train_X[:, 1:]) # exclude the first column (time)
+# test_features = extract_features(test_X[:, 1:]) # exclude the first column (time)
+# training_labels = np.concatenate((np.zeros((walking_filtered.shape[0], 1)),
+#                                   np.ones((jumping_filtered.shape[0], 1))), axis=0)
+# test_labels = np.concatenate((np.zeros((test_walking_features.shape[0], 1)),
+#                               np.ones((test_jumping_features.shape[0], 1))), axis=0)
 
 
+# print(train_features)
+# print(test_features)
 
-# # Save data to HDF5 file
-with h5py.File('combined_data.hdf5', 'w') as f:
-    # Store training data
-    f.create_dataset('train_X', data=train_data.values)
-    f.create_dataset('train_y', data=np.zeros((len(train_data), 1)))
+# # Train a logistic regression model
+# l_reg = LogisticRegression(max_iter=10000)
+# scaler = StandardScaler()
+# clf = make_pipeline(scaler,l_reg)
+# clf.fit(train_features, train_y)
 
-    # Store test data
-    f.create_dataset('test_X', data=test_data.values)
-    f.create_dataset('test_y', data=np.zeros((len(test_data), 1)))
-
-# Load data from HDF5 file
-with h5py.File('combined_data.hdf5', 'r') as f:
-    train_X = f['train_X'][:]
-    train_y = f['train_y'][:]
-    test_X = f['test_X'][:]
-    test_y = f['test_y'][:]
-
-# Apply moving average filter
-# train_X = savgol_filter(train_X, window_length=5, polyorder=3, axis=1)
-# test_X = savgol_filter(test_X, window_length=5, polyorder=3, axis=1)
-
-# # Detect and remove outliers
-# train_X = train_X[~((train_X - np.mean(train_X, axis=1, keepdims=True)) > 2 * np.std(train_X, axis=1, keepdims=True))]
-# train_y = train_y[~((train_X - np.mean(train_X, axis=1, keepdims=True)) > 2 * np.std(train_X, axis=1, keepdims=True))]
-# test_X = test_X[~((test_X - np.mean(test_X, axis=1, keepdims=True)) > 2 * np.std(test_X, axis=1, keepdims=True))]
-# test_y = test_y[~((test_X - np.mean(test_X, axis=1, keepdims=True)) > 2 * np.std(test_X, axis=1, keepdims=True))]
-
-# # Normalize the data
-# train_X = (train_X - np.mean(train_X)) / np.std(train_X)
-# test_X = (test_X - np.mean(test_X)) / np.std(test_X)
-
-# Define a function to extract features
-def extract_features(data):
-    features = []
-    for i in range(data.shape[0]):
-        window = data[i, :, :]
-        feature = []
-        feature.append(np.max(window[:, 0]))  # Maximum acceleration in x-direction
-        feature.append(np.max(window[:, 1]))  # Maximum acceleration in y-direction
-        feature.append(np.max(window[:, 2]))  # Maximum acceleration in z-direction
-        feature.append(np.min(window[:, 0]))  # Minimum acceleration in x-direction
-        feature.append(np.min(window[:, 1]))  # Minimum acceleration in y-direction
-        feature.append(np.min(window[:, 2]))  # Minimum acceleration in z-direction
-        feature.append(np.mean(window[:, 0]))  # Mean acceleration in x-direction
-        feature.append(np.mean(window[:, 1]))  # Mean acceleration in y-direction
-        feature.append(np.mean(window[:, 2]))  # Mean acceleration in z-direction
-        feature.append(np.std(window[:, 0]))  # Standard deviation of acceleration in x-direction
-        feature.append(np.std(window[:, 1]))  # Standard deviation of acceleration in y-direction
-        feature.append(np.std(window[:, 2]))  # Standard deviation of acceleration in z-direction
-        features.append(feature)
-    return np.array(features)
-
-# Extract features from training and testing sets
-train_features = extract_features(train_X)
-test_features = extract_features(test_X)
-
-# Train a logistic regression model
-# clf = LogisticRegression(random_state=42).fit(train_features, train_y)
-
-# # Predict labels for test set
+# # # Predict labels for test set
 # y_pred = clf.predict(test_features)
 
-# # Calculate accuracy score
+# # # Calculate accuracy score
 # accuracy = accuracy_score(test_y, y_pred)
 
 # print(f"Test accuracy: {accuracy:.2f}")
